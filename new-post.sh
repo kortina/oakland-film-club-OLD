b1e08fcf8889f6dbbcb51da7a60355248d703899
Kortina <kortina@gmail.com>
2018-08-25 17:10:09 -0700

    add helper for generating a new post

---
+++ new-post.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+
+if [ -z "$1" ]; then
+    echo "Missing required argument, post title:"
+    echo "./new-post.sh \"#n // Your Movie Title\""
+    exit 1
+fi
+
+title="$1"
+t="`echo "$title" | iconv -t ascii//TRANSLIT | sed -E 's/[~\^]+//g' | sed -E 's/[^a-zA-Z0-9]+/-/g' | sed -E 's/^-+\|-+$//g' | tr A-Z a-z`"
+f="_posts/`date +'%Y-%m-%d'`-film-club$t.md"
+
+cat > $f <<- EOM
+---
+layout: post
+title: "Film Club $title"
+author: `id -un`
+---
+
+***Pre***
+
+..
+
+***Post***
+
+..
+
+EOM
+
+echo "Created new post:"
+echo $f
