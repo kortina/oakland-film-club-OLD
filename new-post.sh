10e4ff661b1e8575df4ccbbec34a98687813725f
kortina <kortina@gmail.com>
2020-08-21 20:16:18 -0700

    update new post script

--- new-post.sh
+++ new-post.sh
@@ -2,7 +2,7 @@
 
 if [ -z "$1" ]; then
     echo "Missing required argument, post title:"
-    echo "./new-post.sh \"#n // Your Movie Title\""
+    echo "./new-post.sh \"Your Movie Title\""
     echo "Last post:"
     ls -1 _posts | tail -n -1
     exit 1
@@ -10,12 +10,12 @@ fi
 
 title="$1"
 t="`echo "$title" | iconv -t ascii//TRANSLIT | sed -E 's/[~\^]+//g' | sed -E 's/[^a-zA-Z0-9]+/-/g' | sed -E 's/^-+\|-+$//g' | tr A-Z a-z`"
-f="_posts/`date +'%Y-%m-%d'`-film-club$t.md"
+f="_posts/`date +'%Y-%m-%d'`-$t.md"
 
 cat > $f <<- EOM
 ---
 layout: post
-title: "Film Club $title"
+title: "$title"
 author: `id -un`
 ---
 
